7e6069e224ed86bc8348f0ebd14386b84a847b19
曲磊 <joshqu1985@gmail.com>
2018-11-20 17:22:12 +0800

    fx

--- board/src/db.go
+++ board/src/db.go
@@ -1,3 +1,116 @@
 package main
 
-//
+import (
+	"sort"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
+	"a.com/go-server/common/mongo"
+)
+
+type DBHandle struct {
+}
+
+var DB = &DBHandle{}
+
+func (db *DBHandle) ListFirstComms(oid, direct string,
+	stamp int64, limit int) (CommRecordList, error) {
+	items := make(CommRecordList, 0)
+	handle := func(c *mgo.Collection) error {
+		query := bson.M{"oid": oid, "status": 0, "level": 1}
+		if len(direct) != 0 {
+			query["created_at"] = bson.M{"$" + direct: stamp} //$gt, $lt
+		}
+
+		sorter := "-created_at"
+		if direct == "gt" || direct == "gte" {
+			sorter = "created_at"
+		}
+
+		err := c.Find(query).Sort(sorter).Limit(limit).All(&items)
+		if len(items) > 0 && sorter == "-created_at" {
+			sort.Sort(items)
+		}
+		return err
+	}
+	return items, mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) ListChildComms(oid, cid, direct string,
+	stamp int64, limit int) (CommRecordList, error) {
+	items := make(CommRecordList, 0)
+	handle := func(c *mgo.Collection) error {
+		query := bson.M{"oid": oid, "status": 0, "level": 2, "cid": cid}
+		if len(direct) != 0 {
+			query["created_at"] = bson.M{"$" + direct: stamp} //$gt, $lt
+		}
+
+		sorter := "-created_at"
+		if direct == "gt" || direct == "gte" {
+			sorter = "created_at"
+		}
+
+		err := c.Find(query).Sort(sorter).Limit(limit).All(&items)
+		if len(items) > 0 && sorter == "-created_at" {
+			sort.Sort(items)
+		}
+		return err
+	}
+	return items, mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) MGetComments(ids []string) (CommRecordList, error) {
+	items := make(CommRecordList, 0)
+	handle := func(c *mgo.Collection) error {
+		return c.Find(bson.M{"_id": bson.M{"$in": ids}}).All(&items)
+	}
+	return items, mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) GetComment(id string) (CommRecord, error) {
+	item := CommRecord{}
+	handle := func(c *mgo.Collection) error {
+		return c.Find(bson.M{"_id": id}).One(&item)
+	}
+	return item, mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) NewComment(item CommRecord) error {
+	handle := func(c *mgo.Collection) error {
+		return c.Insert(item)
+	}
+	return mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) DelComment(id string) error {
+	handle := func(c *mgo.Collection) error {
+		return c.Update(bson.M{"_id": id},
+			bson.M{"$set": bson.M{"status": DELETE_STATUS}})
+	}
+	return mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) IncrCommReply(id string) error {
+	return nil
+}
+
+func (db *DBHandle) DecrCommReply(id string) error {
+	return nil
+}
+
+func (db *DBHandle) IncrCommLike(id string) error {
+	handle := func(c *mgo.Collection) error {
+		return c.Update(bson.M{"_id": id},
+			bson.M{"$inc": bson.M{"likes_count": 1}})
+	}
+	return mongo.Doit("Comment", "comment", handle)
+}
+
+func (db *DBHandle) DecrCommLike(id string) error {
+	handle := func(c *mgo.Collection) error {
+		return c.Update(bson.M{"_id": id},
+			bson.M{"$inc": bson.M{"likes_count": -1}})
+	}
+	return mongo.Doit("Comment", "comment", handle)
+}
--- board/src/st.go
+++ board/src/st.go
@@ -1,6 +1,11 @@
 package main
 
-type CommentRecord struct {
+const (
+	NORMAL_STATUS = 0
+	DELETE_STATUS = 1
+)
+
+type CommRecord struct {
 	Id    string `json:"id" bson:"_id,omitempty"`
 	Oid   string `json:"oid" bson:"oid"`
 	Cid   string `json:"cid" bson:"cid"`
@@ -20,7 +25,21 @@ type CommentRecord struct {
 	ReplyCount int64 `json:"reply_count" bson:"reply_count"`
 	CreatedAt  int64 `json:"created_at" bson:"created_at"`
 
-	LatestReplys []CommentRecord `json:"latest_replys" bson:"latest_replys"`
+	LatestReplys []CommRecord `json:"latest_replys" bson:"latest_replys"`
+}
+
+type CommRecordList []CommRecord
+
+func (c CommRecordList) Len() int {
+	return len(c)
+}
+
+func (c CommRecordList) Less(i, j int) bool {
+	return c[i].CreatedAt < c[j].CreatedAt
+}
+
+func (c CommRecordList) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
 }
 
 type CommLikeRecord struct {
